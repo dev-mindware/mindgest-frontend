796e3d6ac86e97e58185f72e1ac1bb7ec24ae19c
Jonatao-Cardoso <147612467+WildKnuckles@users.noreply.github.com>
2025-08-18 10:35:00 +0100

    product-form validations 1st

---
+++ src/components/templates/products/product-modals/constant-data.ts
@@ -0,0 +1,13 @@
+export const measurement = [
+  { label: "Unidade", value: "un" },
+  { label: "Pacote", value: "pct" },
+  { label: "Caixa", value: "cx" },
+  { label: "Dúzia", value: "dz" },
+  { label: "Par", value: "par" },
+];
+
+export const category = [
+  { label: "Eletrónicos", value: "elt"},
+  { label: "Alimentos", value: "alc"},
+  { label: "Cosméticos", value: "cosm"}
+]
--- src/schemas/add-product.ts
+++ src/schemas/add-product.ts
@@ -3,22 +3,23 @@ import { z } from "zod";
 export const addProductSchema = z.object({
   name: z.string().nonempty("Campo obrigatório"),
   selectedCategory: z.string().nonempty("Campo obrigatório"),
+  sku: z.string().nonempty("Campo obrigatório"),
+  price: z.coerce.number().min(0, "Preço deve ser maior ou igual a 0"),
 
   selectedMeasurement: z.string().optional(),
   supplier: z.string().optional(),
   location: z.string().optional(),
 
-  price: z.coerce.number().min(0, "Preço deve ser maior ou igual a 0").optional().nullable(),
   initialStock: z.coerce.number().min(0, "Estoque inicial deve ser maior ou igual a 0").optional().nullable(),
   minStock: z.coerce.number().min(0, "Estoque mínimo deve ser maior ou igual a 0").optional().nullable(),
 
-  expiryDate: z.string().datetime().optional().nullable(),
+  expiryDate: z.string().optional().nullable(),
   tax: z.coerce.number().min(0, "Imposto deve ser maior ou igual a 0").optional().nullable(),
   warranty: z.coerce.number().optional().nullable(),
   repositionTime: z.coerce.number().min(0, "Tempo de reposição deve ser maior ou igual a 0").optional().nullable(),
   salesPerDay: z.coerce.number().min(0, "Vendas por dia deve ser maior ou igual a 0").optional().nullable(),
 
-  description: z.string().max(500, "Descrição deve ter no máximo 500 caracteres").optional()
+  description: z.string().max(500, "Descrição deve ter no máximo 500 caracteres").optional(),
 });
 
 export type AddProductFormData = z.infer<typeof addProductSchema>;
